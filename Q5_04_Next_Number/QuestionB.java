c5a609008fb394b2e5c2addcae322233ab14356e
Ahmad Obiedat <obieda01@email.franklin.edu>
2016-07-16 15:24:27 -0400

    Update QuestionB.java

--- Q5_04_Next_Number/QuestionB.java
+++ Q5_04_Next_Number/QuestionB.java
@@ -1,4 +1,16 @@
+package Q5_04_Next_Number;
 
+public class QuestionB {
+	
+	public static int getNext(int n) {
+		int c = n;
+		int c0 = 0;
+		int c1 = 0;
+		while (((c & 1) == 0) && (c != 0)) {
+			c0++;
+			c >>= 1;
+		}
+		
 		while ((c & 1) == 1) {
 			c1++;
 			c >>= 1;
